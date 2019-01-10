33f8ab4dbed202d5c35e96b9e9249da2c0765dd7
Max Crofts <maxcrofts@users.noreply.github.com>
2019-01-10 18:34:19 +1000

    Ensure maptexture_t remains a consistent size

--- doomclassic/doom/structs.h
+++ doomclassic/doom/structs.h
@@ -348,7 +348,7 @@ typedef struct
     int			masked;	
     short		width;
     short		height;
-    void		**columndirectory;	// OBSOLETE
+    int			columndirectory;	// OBSOLETE
     short		patchcount;
     mappatch_t	patches[1];
 } maptexture_t;
