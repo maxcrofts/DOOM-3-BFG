11763a75f54e962538dc52cec4f23bc2b7cb65f5
Max Crofts <maxcrofts@users.noreply.github.com>
2019-01-21 10:09:47 +1000

    Add standard vsnprintf

--- neo/idlib/Str.cpp
+++ neo/idlib/Str.cpp
@@ -1846,9 +1846,15 @@ or returns -1 on failure or if the buffer would be overflowed.
 int idStr::vsnPrintf( char *dest, int size, const char *fmt, va_list argptr ) {
 	int ret;
 
+#ifdef ID_WIN
 #undef _vsnprintf
 	ret = _vsnprintf( dest, size-1, fmt, argptr );
 #define _vsnprintf	use_idStr_vsnPrintf
+#else
+#undef vsnprintf
+	ret = vsnprintf( dest, size, fmt, argptr );
+#define vsnprintf	use_idStr_vsnPrintf
+#endif
 	dest[size-1] = '\0';
 	if ( ret < 0 || ret >= size ) {
 		return -1;
