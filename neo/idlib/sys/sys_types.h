3b7729fb8e3fecb921c615c764716d3da4813c0a
Max Crofts <maxcrofts@users.noreply.github.com>
2019-07-30 14:44:02 +1000

    Remove custom nullptr
    This would cause issues if the defines added using CMake's compiler feature detection didn't match what was implied by the value of __cplusplus. Additionally, nullptr isn't currently used anywhere in the codebase so it doesn't really make sense to provide a shim for pre-C++11 compilers.

--- neo/idlib/sys/sys_types.h
+++ neo/idlib/sys/sys_types.h
@@ -98,25 +98,6 @@ private:
 	_type_ *ptr;
 };
 
-#ifndef ID_COMPILER_CXX_CONSTEXPR
-#define constexpr
-#endif
-
-#ifndef ID_COMPILER_CXX_NULLPTR
-struct idNullPtr {
-	// one pointer member initialized to zero so you can pass NULL as a vararg
-	void *value; idNullPtr() : value( 0 ) { }
-
-	// implicit conversion to all pointer types
-	template<typename T1> constexpr operator T1 * () const { return 0; }
-
-	// implicit conversion to all pointer to member types
-	template<typename T1, typename T2> operator T1 T2::* () const { return 0; }
-};
-
-#define nullptr					idNullPtr()
-#endif
-
 //#undef NULL
 //#if defined( ID_WIN ) && !defined( ID_TOOL_EXTERNAL ) && !defined( _lint )
 //#define NULL					idNullPtr()
